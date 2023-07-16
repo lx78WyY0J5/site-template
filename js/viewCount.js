7c1a7ef0200826a0d8da82b4885130a6ade6ca88
lx78WyY0J5 <9j2k37st@gmail.com>
2023-07-16 18:13:35 +0200

    View count

--- js/viewCount.js
+++ js/viewCount.js
@@ -1,61 +1,28 @@
-setViewCount();
-
 async function setViewCount() {
-    var global = await gatherJSON('https://hits.dwyl.com\/lx78wyy0j5\/lx78wyy0j5githubio.json');
-
-    console.log(global);
-
-    var global = await gatherJSON('https://hits.dwyl.com/lx78wyy0j5/lx78wyy0j5githubio.json');
-    var globalHit = getValue(global[0], "message");
-    document.getElementById("total-number").textContent = globalHit;
-
-    var globalUnique = await gatherJSON('https://hits.dwyl.com/lx78wyy0j5/lx78wyy0j5githubio.json?show=unique');
-    var globalUniqueHit = getValue(globalUnique[0], "message");
-    document.getElementById("unique-total-number").textContent = globalUniqueHit;
+    var global = await getPromise("lx78wyy0j5/lx78wyy0j5.github.io");
+    document.getElementById("global-viewcount").textContent = global;
 
     var pathname = window.location.pathname;
-    document.getElementById("page-name").textContent = pathname;
     var pathnameDot = pathname.replace("/", ".");
     var pathnameHTML = pathnameDot.replace(".html", "");
 
-    var globalPage = await gatherJSON('https://hits.dwyl.com/lx78wyy0j5/lx78wyy0j5githubio' + pathnameHTML + '.json');
-    var globalPageHit = getValue(globalPage[0], "message");
-    document.getElementById("total-page-number").textContent = globalPageHit;
+    var globalPage = await getPromise("lx78wyy0j5/lx78wyy0j5.github.io" + pathnameHTML);
+    document.getElementById("page-name").textContent = pathname.replace(".html", "");
+    document.getElementById("page-viewcount").textContent = globalPage;
 
-    var globalPageUnique = await gatherJSON('https://hits.dwyl.com/lx78wyy0j5/lx78wyy0j5githubio' + pathnameHTML + '.json?show=unique');
-    var globalPageUniqueHit = getValue(globalPageUnique[0], "message");
-    document.getElementById("unique-total-page-number").textContent = globalPageUniqueHit;
+    var globalPageUnique = globalPage + global;
+    document.getElementById("total-viewcount").textContent = globalPageUnique;
 }
 
-function gatherJSON(url) {
-    return new Promise(function (resolve, reject) {
-        setTimeout(() => {
-            fetch(url, {
-                method: 'GET',
-                mode: 'no-cors',
-                headers: {
-                    "Content-Type": "application/json",
-                    'Access-Control-Allow-Origin': '*',
-                },
-            })
-                .then(data => {
-                    resolve(data);
-                    console.log(data);
-                    return;
-                })
-                .catch(error => console.error(error))
-        }, 1000);
-    });
-}
-
-function getValue(obj, key) {
-    var value;
-    for (var i in obj) {
-        if (!obj.hasOwnProperty(i)) continue;
-        if (i == key) {
-            value = obj[i];
-            break;
-        }
-    }
+const getPromise = async (repo, callback) => {
+    let data = await fetch("https://visit-counter.vercel.app/counter?page=" + repo); console.log(data);
+    let value = await data.json(); console.log(value);
     return value;
-}  
+}
+
+const getActivity = async () => {
+    let count = await getPromise(repo);
+    console.log("fin : " + count);
+}
+
+setViewCount();
