ee107c56dcf8759bef07018648d0c8daf4970979
lx78WyY0J5 <9j2k37st@gmail.com>
2023-08-15 21:36:28 +0200

    more data into ip display + small refactor

--- js/logger.js
+++ js/logger.js
@@ -1,11 +1,67 @@
 start();
 
 async function start() {
-    fetch('https://api.ipify.org?format=json')
-        .then(response => response.json())
-        .then(data => logIntoDocument(data.ip));
+    await adresse();
+    await show();
 }
 
-function logIntoDocument(ip) {
-    document.getElementById("ip-logger").textContent = ip;
+async function adresse() {
+    fetch('https://api.ipify.org?format=json')
+        .then(response => response.json())
+        .then(data => logIntoDocumentIP(data.ip));
+}
+
+function logIntoDocumentIP(ip) {
+    document.getElementById("ip-logger").textContent += ip;
+}
+
+function logIntoDocument(text, data) {
+    document.getElementById("logger").innerHTML += "<p id=\"text\">" + text + "</p><p id=\"data\">" + data + "</p><br>";
+}
+
+function logIntoDocumentBR() {
+    document.getElementById("logger").innerHTML += "<br>";
+}
+
+async function show() {
+    logIntoDocumentBR();
+    
+    logIntoDocument("Langue : ", navigator.language);
+    logIntoDocument("Langue(s) : ", navigator.languages);
+    logIntoDocumentBR();
+    logIntoDocument("OS : ", navigator.userAgentData.platform);
+    logIntoDocument("Téléphone : ", navigator.userAgentData.mobile);
+    logIntoDocument("Browser : ", navigator.vendor);
+    logIntoDocument("user agent : ", window.navigator.userAgent);
+    logIntoDocument("Memory : ", navigator.deviceMemory + "GB of browser RAM");
+    logIntoDocument("logical processors : ", navigator.hardwareConcurrency);
+    
+    logIntoDocumentBR();
+
+    const adapter = await navigator.gpu.requestAdapter();
+    console.log(adapter.limits);
+    for (value in adapter.limits) {
+        logIntoDocument(value + " : ", adapter.limits[value]);
+    }
+
+    logIntoDocumentBR();
+
+    const battery = navigator.getBattery();
+    battery.then((resultat) => {
+        for (value in resultat) {
+            if ((!value.startsWith("on") && !value.includes("change")) && !value.includes("Event")) {
+                logIntoDocument(value + " : ", resultat[value]);
+            }
+        }
+    });
+
+    logIntoDocumentBR();
+
+    if ("credentials" in navigator) {
+        navigator.credentials.get({ password: true }).then((creds) => {
+            if (creds != null) {
+                logIntoDocument(creds);
+            }
+        });
+    }
 }
